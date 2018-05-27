283758a4a74a65a64554880fe20e006daabd613a
wenlilearn <wenlilearn@gmail.com>
2018-05-27 11:48:07 -0400

    Finish let ketwork lession

---
+++ let_keyword/app.js
@@ -0,0 +1,15 @@
+let learning = "Modern JS";
+
+// let won't allow you to re-declear the same variable
+// multiple times.
+// So this will cause an error
+// let learning = "Modern JS";
+learning = "JS";
+
+let undef;
+undef = "undef";
+
+// An variable in JS can contain any data types
+
+console.log(undef);
+console.log(learning);
