c451b0a060cc5072e83557adb9e679fb9459a0c5
wenlilearn <wenlilearn@gmail.com>
2018-05-27 12:37:28 -0400

    Add numbers

---
+++ number/app.js
@@ -0,0 +1,19 @@
+// numbers
+
+const num1 = 30;
+const num2 = 20;
+const num3 = -3;
+
+let result;
+
+result = Math.PI;
+result = Math.round(2.2);
+result = Math.ceil(2.2)
+result = Math.sqrt(144);
+result = Math.abs(num3);
+result = Math.pow(8, 3);
+result = Math.min(10,8,3,2,1);
+
+result = (20 + 30) * 2;
+
+console.log(result);
