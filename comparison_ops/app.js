993c86961ea6c0e128883333678a628b4d27f9a7
wenlilearn <wenlilearn@gmail.com>
2018-05-28 13:30:32 -0400

    Finish comparison_ops

---
+++ comparison_ops/app.js
@@ -0,0 +1,14 @@
+//equality
+console.log(2 == 2);
+// strict comparision ops: Will check the type also
+console.log(2 === 2);
+console.log(2 === '2');
+
+// not equality
+console.log(2 != '2');
+console.log(2 !== '2');
+
+// null/undefined
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(typeof undefined);
